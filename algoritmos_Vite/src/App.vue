1b2ae0e61dd10f1534ac5c047823c3c1a3d9f3fd
AMorQ <albamorq@gmail.com>
2025-06-23 13:50:17 +0200

    add exercise 18

--- algoritmos_Vite/src/App.vue
+++ algoritmos_Vite/src/App.vue
@@ -30,7 +30,7 @@ export default {
   
     return {
       // message: 'Welcome!'
-      items: ["ejercicio1", "ejercicio2", "ejercicio3", "ejercicio4", "ejercicio5", "ejercicio6", "ejercicio7", "ejercicio8", "ejercicio9", "ejercicio10", "ejercicio11", "ejercicio12", "ejercicio13", "ejercicio14", "ejercicio15", "ejercicio16", "ejercicio17"],
+      items: ["ejercicio1", "ejercicio2", "ejercicio3", "ejercicio4", "ejercicio5", "ejercicio6", "ejercicio7", "ejercicio8", "ejercicio9", "ejercicio10", "ejercicio11", "ejercicio12", "ejercicio13", "ejercicio14", "ejercicio15", "ejercicio16", "ejercicio17", "ejercicio18"],
       currentIndex: 0,
     };
   },
