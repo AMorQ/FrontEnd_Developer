16064adfe2cc7c00bbc81c960729edd27c08c04b
AMorQ <albamorq@gmail.com>
2025-06-23 12:52:05 +0200

    adding exercise 14, the character ñ has to be included (WIP)

--- algoritmos_Vite/src/App.vue
+++ algoritmos_Vite/src/App.vue
@@ -30,7 +30,7 @@ export default {
   
     return {
       // message: 'Welcome!'
-      items: ["ejercicio1", "ejercicio2", "ejercicio3", "ejercicio4", "ejercicio5", "ejercicio6", "ejercicio7", "ejercicio8", "ejercicio9", "ejercicio10", "ejercicio11", "ejercicio12", "ejercicio13"],
+      items: ["ejercicio1", "ejercicio2", "ejercicio3", "ejercicio4", "ejercicio5", "ejercicio6", "ejercicio7", "ejercicio8", "ejercicio9", "ejercicio10", "ejercicio11", "ejercicio12", "ejercicio13", "ejercicio14"],
       currentIndex: 0,
     };
   },
